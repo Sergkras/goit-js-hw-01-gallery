c4176a101ec2566a482a798e3792b4ec13db5ee6
Sergkras <72658674+Sergkras@users.noreply.github.com>
2021-03-14 20:33:54 +0200

    Update fun-modal.js

--- js/fun-modal.js
+++ js/fun-modal.js
@@ -1,1 +1,16 @@
+export default
 
+function addArrGaller(gallery) {
+    const arrGal = gallery.map(({ original, preview, description }) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  });
+    return arrGal.join("");
+};
